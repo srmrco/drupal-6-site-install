6a85fef8b0711db794d8a46cad11843a25949e09
Boris Aprelov <atronax@gmail.com>
2014-05-02 19:44:37 +0400

    fix setting permissions

--- create_platform.sh
+++ create_platform.sh
@@ -37,8 +37,8 @@ new_platform_path=$target_dir/$platform_alias
 echo "Copying new platform to $new_platform_path..."
 sudo cp -r $source_dir $new_platform_path
 
-#echo "Setting permission to aegir:aegir for $new_platform_path..."
-#sudo chown aegir:aegir $new_platform_path
+echo "Setting permission to aegir:aegir for $new_platform_path..."
+sudo chown -R aegir:aegir $new_platform_path
 
 echo "Setting up a new platform in Aegir..."
 $DRUSH --root="$new_platform_path" provision-save "@$platform_alias" --context_type='platform'
