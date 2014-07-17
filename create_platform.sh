015143d24a7a77759af69a14435a927c21fd27d3
Boris <atronax@gmail.com>
2014-07-17 22:57:17 +0400

    no need to sudo copy commands anymore

--- create_platform.sh
+++ create_platform.sh
@@ -19,19 +19,19 @@ platform_alias=platform_$timestamp
 new_platform_path=$target_dir/$platform_alias
 
 echo "Copying new platform to $new_platform_path..."
-sudo cp -r $source_dir $new_platform_path
+cp -r $source_dir $new_platform_path
 
 echo "Setting permission to aegir:aegir for $new_platform_path..."
-sudo chown -R aegir:aegir $new_platform_path
+chown -R aegir:aegir $new_platform_path
 
 echo "Setting up a new platform in Aegir..."
 $DRUSH --root="$new_platform_path" provision-save "@$platform_alias" --context_type='platform'
 
-echo 'Sleeping for 5 seconds...'
+echo 'Sleeping for 10 seconds...'
 sleep 10s
 
 $DRUSH @hostmaster hosting-import "@$platform_alias"
-echo 'Sleeping for 5 more seconds...'
+echo 'Sleeping for 10 more seconds...'
 sleep 10s
 
 echo 'Dispatching frontend update so that platform will become visible in Aegir...'
